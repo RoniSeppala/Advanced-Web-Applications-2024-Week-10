f84841997e8de4609fd10555539cd7d455c072c4
Roni Seppälä <roni.o.seppala@gmail.com>
2025-01-27 15:13:16 +0200

    refactor App component to simplify structure and improve readability

--- src/App.tsx
+++ src/App.tsx
@@ -5,26 +5,12 @@ import Header from './components/Header'
 import MyContainer from './components/MyContainer'
 
 function App() {
-  const [count, setCount] = useState(1)
-
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 2)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-        <About />
-        <Header />
-        <MyContainer />
-      </p>
-    </>
+    <div>
+      <About />
+      <Header />
+      <MyContainer />
+    </div>
   )
 }
 
