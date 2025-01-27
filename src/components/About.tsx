fb96a60c7b7253bc4069de4510b06aefe490d58c
Roni Seppälä <roni.o.seppala@gmail.com>
2025-01-27 16:09:47 +0200

    add i18next and related dependencies; implement data fetching in About component

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,9 +5,32 @@ interface aboutProps {
 }
 
 const About:React.FC<aboutProps> = () => {
+    interface Post {
+        id: number;
+        title: string;
+        body: string;
+    }
+
+    const [fetchData, setFetchData] = React.useState<Post[]>([]);
+
+    React.useEffect(() => {
+        fetch(`https://jsonplaceholder.typicode.com/posts`)
+        .then(response => response.json())
+        .then(json => setFetchData(json))
+    }, []);
+
+
   return (
     <div>
-        This is some random text inside the Header component. 1
+        <h1>About</h1>
+        <ul>
+            {fetchData.map((data) => (
+                <div key={data.id}>
+                    <h3>{data.title}</h3>
+                    <p>{data.body}</p>
+                </div>
+            ))}
+        </ul>
     </div>
   );
 };
